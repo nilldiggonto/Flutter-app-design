041d3cca8240f5390daee0f3ef5cd37b8af729af
nilldiggonto <nilldiggonto@gmail.com>
2021-06-15 03:07:15 +0600

    Http Response

--- intro_app/lib/main.dart
+++ intro_app/lib/main.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MaterialApp(
       home: HomePage(),
@@ -13,12 +15,94 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // Future<String> makeRequest() async{
+  List? data;
 
-  // }
+  Future<void> makeRequest() async {
+    String url = 'https://randomuser.me/api/?results=15';
+    var response =
+        await http.get(Uri.parse(url), headers: {'Accept': 'application/json'});
+
+    // Map
+    setState(() {
+      var extractData = json.decode(response.body);
+      data = extractData['results'];
+    });
+
+    // print(data[0]['name']['first']);
+
+    // print(response.body);
+    // var a = response.body;
+    // return '';R
+    // return response;
+  }
+
+  @override
+  void initState() {
+    this.makeRequest();
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('User List'),
+      ),
+      body: ListView.builder(
+          itemCount: data == null ? 0 : data!.length,
+          itemBuilder: (context, i) {
+            return ListTile(
+              title: Text(data![i]['name']['first']),
+              subtitle: Text(data![i]['phone']),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  data![i]['picture']['thumbnail'],
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AnotherPage(data![i]),
+                  ),
+                );
+              },
+            );
+          }),
+    );
+  }
+}
+
+class AnotherPage extends StatelessWidget {
+  // const AnotherPage({Key? key}) : super(key: key);
+  AnotherPage(this.data);
+  final data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('User Detail page'),
+        ),
+        body: Center(
+          child: Container(
+            width: 150.0,
+            height: 150.0,
+            decoration: BoxDecoration(
+                color: Color(0xff7c94b6),
+                image: DecorationImage(
+                  image: NetworkImage(data!['picture']['large']),
+                  fit: BoxFit.cover,
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(75.0),
+                ),
+                border: Border.all(
+                  color: Colors.amber,
+                  width: 4.0,
+                )),
+          ),
+        ));
   }
 }
