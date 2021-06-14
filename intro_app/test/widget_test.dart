755394a79dbfd5f8b31783e050dee17a6fe593c2
nilldiggonto <nilldiggonto@gmail.com>
2021-06-14 21:03:25 +0600

    Intro to flutter

--- intro_app/test/widget_test.dart
+++ intro_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app_two/main.dart';
+import 'package:intro_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
