47bb11498b2546d4bd07793a4a2b18e8621ccbad
nilldiggonto <nilldiggonto@gmail.com>
2021-06-14 22:32:57 +0600

    Working with http

--- intro_app/lib/main.dart
+++ intro_app/lib/main.dart
@@ -1,13 +1,8 @@
-// import 'package:flutter_test/flutter_test.dart'
 import 'package:flutter/material.dart';
-import 'package:intro_app/firstPage.dart';
-import 'package:intro_app/secondPage.dart';
+import 'package:http/http.dart';
 
 void main() => runApp(MaterialApp(
       home: HomePage(),
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
     ));
 
 class HomePage extends StatefulWidget {
@@ -17,53 +12,13 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  TabController? controller;
+class _HomePageState extends State<HomePage> {
+  // Future<String> makeRequest() async{
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    controller = TabController(vsync: this, length: 2);
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    controller!.dispose();
-    super.dispose();
-  }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Working with Tab'),
-        backgroundColor: Colors.teal,
-        // bottom:
-      ),
-      bottomNavigationBar: Material(
-        color: Colors.teal,
-        child: TabBar(
-          controller: controller,
-          tabs: [
-            Tab(
-              icon: Icon(Icons.access_alarm),
-            ),
-            Tab(
-              icon: Icon(Icons.access_time),
-            )
-          ],
-        ),
-      ),
-      body: TabBarView(
-        controller: controller,
-        children: [
-          FirstPage(),
-          SecondPage(),
-        ],
-      ),
-    );
+    return Scaffold();
   }
 }
