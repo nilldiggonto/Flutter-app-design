769a9379155fafe186c3bb1dae1e8726154918cc
nilldiggonto <nilldiggonto@gmail.com>
2021-06-16 22:46:32 +0600

    Travel App with PageView

--- travel_app/lib/main.dart
+++ travel_app/lib/main.dart
@@ -1,1 +1,213 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  PageController? _pageController;
+  int totalPage = 4;
+
+  void _onscroll() {
+    // print('on scroll');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _pageController = PageController(
+      initialPage: 0,
+    )..addListener(_onscroll);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        children: [
+          makePage(
+              page: 1,
+              image: 'assets/images/one.jpg',
+              title: 'BD',
+              description:
+                  'This is blah blah.This is blah blah.This is blah blah.This is blah blah'),
+          makePage(
+              page: 2,
+              image: 'assets/images/two.jpg',
+              title: 'USA',
+              description:
+                  'This is blah blah.This is blah blah.This is blah blah.This is blah blah'),
+          makePage(
+              page: 3,
+              image: 'assets/images/three.jpg',
+              title: 'DUBAI',
+              description:
+                  'This is blah blah.This is blah blah.This is blah blah.This is blah blah'),
+          makePage(
+              page: 4,
+              image: 'assets/images/four.jpg',
+              title: 'SRILANKA',
+              description:
+                  'This is blah blah.This is blah blah.This is blah blah.This is blah blah'),
+        ],
+      ),
+    );
+  }
+
+  Widget makePage({image, title, description, page}) {
+    return Container(
+      // color: Colors.red,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(image),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.bottomRight,
+            stops: [0.3, 0.9],
+            colors: [
+              Colors.black.withOpacity(.7),
+              Colors.black.withOpacity(.2)
+            ],
+          ),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 40.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  Text(
+                    page.toString(),
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '/' + totalPage.toString(),
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 5),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.grey,
+                            size: 15,
+                          ),
+                        ),
+                        Text(
+                          '4.0',
+                          style: TextStyle(color: Colors.white70),
+                        ),
+                        Text(
+                          '(400)',
+                          style: TextStyle(color: Colors.white38, fontSize: 12),
+                        ),
+                      ],
+                      // Text()
+                    ),
+                    SizedBox(height: 20.0),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 50),
+                      child: Text(
+                        description,
+                        style: TextStyle(
+                          color: Colors.white.withOpacity(.7),
+                          height: 1.3,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Text(
+                      'READ MORE',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
